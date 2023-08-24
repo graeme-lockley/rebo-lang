ee331dac4c71d3ef331aca55def7a57895fcaab5
Graeme Lockley <graeme.lockley@gmail.com>
2023-08-24 16:50:58 +0200

    feature: scanner should return UpperIdentifier tokens

--- components/parser/src/main/kotlin/org/rebo/parser/Scanner.kt
+++ components/parser/src/main/kotlin/org/rebo/parser/Scanner.kt
@@ -16,10 +16,90 @@ class Scanner(private val input: String) {
         return offset >= inputLength
     }
 
+    private fun positionAt(): Position {
+        return Position(line, column, offset)
+    }
+
+    private fun skipWhile(predicate: (Char) -> Boolean) {
+        while (!atEnd()) {
+            if (predicate(input[offset])) {
+                skipCharacter()
+                continue
+            }
+
+            break
+        }
+    }
+
     fun next() {
+        ignoreWhitespace()
+
         if (atEnd()) {
             token = Token(TokenType.EOS, "", Position(line, column, offset))
             return
         }
+
+        val c = input[offset]
+        when {
+            c == ',' -> {
+                token = Token(TokenType.Comma, ",", Position(line, column, offset))
+                skipCharacter()
+                return
+            }
+
+            isUpperAlpha(c) -> {
+                val start = positionAt()
+                skipCharacter()
+                skipWhile { isAlpha(it) || isDigit(it) }
+                token =
+                    Token(TokenType.UpperIdentifier, input.substring(start.offset, offset), Range(start, positionAt()))
+            }
+
+            else -> TODO("Unknown token")
+        }
+
+    }
+
+    private fun ignoreWhitespace() {
+        while (!atEnd()) {
+            val c = input[offset]
+            if (c == ' ' || c == '\t' || c == '\r' || c == '\n') {
+                skipCharacter()
+                continue
+            }
+
+            break
+        }
+    }
+
+    private fun skipCharacter() {
+        if (atEnd()) {
+            return
+        }
+
+        val c = input[offset]
+        if (c == '\n') {
+            column = 1
+            line++
+        } else {
+            column++
+        }
+        offset++
+    }
+
+    private fun isDigit(c: Char): Boolean {
+        return c in '0'..'9'
+    }
+
+    private fun isAlpha(c: Char): Boolean {
+        return c in 'a'..'z' || c in 'A'..'Z'
+    }
+
+    private fun isUpperAlpha(c: Char): Boolean {
+        return c in 'A'..'Z'
+    }
+
+    private fun isLowerAlpha(c: Char): Boolean {
+        return c in 'a'..'z'
     }
 }
--- components/parser/src/test/kotlin/org/rebo/parser/ScannerTest.kt
+++ components/parser/src/test/kotlin/org/rebo/parser/ScannerTest.kt
@@ -3,9 +3,32 @@ package org.rebo.parser
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
+private fun tokens(scanner: Scanner): List<Token> {
+    val tokens = mutableListOf<Token>()
+    while (scanner.token.type != TokenType.EOS) {
+        tokens.add(scanner.token)
+        scanner.next()
+    }
+
+    return tokens
+}
+
+private fun tokenTypesLexemes(scanner: Scanner): List<Pair<TokenType, String>> =
+    tokens(scanner).map { Pair(it.type, it.lexeme) }
+
+private fun tokenTypesLexemes(input: String): List<Pair<TokenType, String>> =
+    tokenTypesLexemes(Scanner(input))
+
 class ScannerTest : FunSpec({
     test("Scanner should return EOS token when input is empty") {
-        val scanner = Scanner("")
-        scanner.token shouldBe Token(TokenType.EOS, "", Position(1, 1, 0))
+        Scanner("").token shouldBe Token(TokenType.EOS, "", Position(1, 1, 0))
+        Scanner("   ").token shouldBe Token(TokenType.EOS, "", Position(1, 4, 3))
+    }
+
+    test("Scanner should return UpperIdentifier tokens") {
+        tokenTypesLexemes("Hello") shouldBe listOf(Pair(TokenType.UpperIdentifier, "Hello"))
+        tokenTypesLexemes("Hello World") shouldBe listOf(Pair(TokenType.UpperIdentifier, "Hello"), Pair(TokenType.UpperIdentifier, "World"))
+        tokenTypesLexemes("  Hello   World   ") shouldBe listOf(Pair(TokenType.UpperIdentifier, "Hello"), Pair(TokenType.UpperIdentifier, "World"))
     }
 })
+
