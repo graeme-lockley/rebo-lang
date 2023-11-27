6c4490641cef41ffdcddd55d2366dfb7e06c18a2
Graeme Lockley <graeme.lockley@gmail.com>
2023-11-27 06:23:24 +0200

    refactor: use getArgument

--- src/builtins/write.zig
+++ src/builtins/write.zig
@@ -1,17 +1,13 @@
 const Helper = @import("./helper.zig");
 
 pub fn write(machine: *Helper.Machine, calleeAST: *Helper.Expression, argsAST: []*Helper.Expression) !void {
-    const handle = machine.memoryState.getFromScope("handle") orelse machine.memoryState.unitValue;
-    const bytes = machine.memoryState.getFromScope("bytes") orelse machine.memoryState.unitValue;
+    const handle = try Helper.getArgument(machine, calleeAST, argsAST, "handle", 0, &[_]Helper.ValueKind{ Helper.ValueValue.FileKind, Helper.ValueKind.StreamKind });
+    const bytes = try Helper.getArgument(machine, calleeAST, argsAST, "bytes", 1, &[_]Helper.ValueKind{Helper.ValueValue.StringKind});
 
-    if (bytes.?.v != Helper.ValueKind.StringKind) {
-        try Helper.reportPositionExpectedTypeError(machine, 1, argsAST, calleeAST.position, &[_]Helper.ValueKind{Helper.ValueValue.StringKind}, bytes.?.v);
-    }
-
-    const bytesWritten: usize = switch (handle.?.v) {
-        Helper.ValueKind.FileKind => handle.?.v.FileKind.file.write(bytes.?.v.StringKind),
-        Helper.ValueKind.StreamKind => handle.?.v.StreamKind.stream.write(bytes.?.v.StringKind),
-        else => return Helper.reportPositionExpectedTypeError(machine, 0, argsAST, calleeAST.position, &[_]Helper.ValueKind{ Helper.ValueValue.FileKind, Helper.ValueValue.StreamKind }, handle.?.v),
+    const bytesWritten: usize = switch (handle.v) {
+        Helper.ValueKind.FileKind => handle.v.FileKind.file.write(bytes.v.StringKind),
+        Helper.ValueKind.StreamKind => handle.v.StreamKind.stream.write(bytes.v.StringKind),
+        else => unreachable,
     } catch |err| return Helper.osError(machine, "write", err);
 
     try machine.memoryState.pushIntValue(@intCast(bytesWritten));
