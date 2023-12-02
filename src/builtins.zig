25621247f8c971a96885c5f113df60d52eb1268e
Graeme Lockley <graeme.lockley@gmail.com>
2023-12-02 07:53:35 +0200

    refactor: move builtin open

--- src/builtins.zig
+++ src/builtins.zig
@@ -38,54 +38,6 @@ fn osError(machine: *Machine, operation: []const u8, err: anyerror) !void {
     try record.v.RecordKind.set(machine.memoryState.allocator, "kind", try machine.memoryState.newOwnedStringValue(&buffer));
 }
 
-fn booleanOption(options: *V.Value, name: []const u8, default: bool) bool {
-    const option = options.v.RecordKind.get(name);
-
-    if (option == null or option.?.v != V.ValueKind.BoolKind) {
-        return default;
-    }
-
-    return option.?.v.BoolKind;
-}
-
-pub fn open(machine: *Machine, calleeAST: *AST.Expression, argsAST: []*AST.Expression) !void {
-    const path = machine.memoryState.getFromScope("path") orelse machine.memoryState.unitValue;
-    const options = machine.memoryState.getFromScope("options") orelse machine.memoryState.unitValue;
-
-    if (path.?.v != V.ValueKind.StringKind) {
-        const position = if (argsAST.len > 0) argsAST[0].position else calleeAST.position;
-        try reportExpectedTypeError(machine, position, &[_]V.ValueKind{V.ValueValue.StringKind}, path.?.v);
-    }
-    if (options.?.v != V.ValueKind.RecordKind and options.?.v != V.ValueKind.VoidKind) {
-        const position = if (argsAST.len > 1) argsAST[1].position else calleeAST.position;
-        try reportExpectedTypeError(machine, position, &[_]V.ValueKind{V.ValueValue.RecordKind}, options.?.v);
-    }
-
-    if (options.?.v == V.ValueKind.VoidKind) {
-        try machine.memoryState.push(try machine.memoryState.newFileValue(std.fs.cwd().openFile(path.?.v.StringKind, .{}) catch |err| return osError(machine, "open", err)));
-        return;
-    }
-
-    const readF = booleanOption(options.?, "read", false);
-    const writeF = booleanOption(options.?, "write", false);
-    const appendF = booleanOption(options.?, "append", false);
-    const truncateF = booleanOption(options.?, "truncate", false);
-    const createF = booleanOption(options.?, "create", false);
-
-    if (createF) {
-        try machine.memoryState.push(try machine.memoryState.newFileValue(std.fs.cwd().createFile(path.?.v.StringKind, .{ .read = readF, .truncate = truncateF, .exclusive = true }) catch |err| return osError(machine, "open", err)));
-    } else {
-        const mode = if (readF and writeF) std.fs.File.OpenMode.read_write else if (readF) std.fs.File.OpenMode.read_only else std.fs.File.OpenMode.write_only;
-        var file = std.fs.cwd().openFile(path.?.v.StringKind, .{ .mode = mode }) catch |err| return osError(machine, "open", err);
-
-        try machine.memoryState.push(try machine.memoryState.newFileValue(file));
-
-        if (appendF) {
-            file.seekFromEnd(0) catch |err| return osError(machine, "open", err);
-        }
-    }
-}
-
 fn printValue(stdout: std.fs.File.Writer, v: *const V.Value) !void {
     switch (v.v) {
         .BoolKind => try stdout.print("{s}", .{if (v.v.BoolKind) "true" else "false"}),
@@ -251,6 +203,7 @@ pub const listen = @import("./builtins/listen.zig").listen;
 pub const len = @import("./builtins/len.zig").len;
 pub const ls = @import("./builtins/ls.zig").ls;
 pub const milliTimestamp = @import("./builtins/milliTimestamp.zig").milliTimestamp;
+pub const open = @import("./builtins/open.zig").open;
 pub const read = @import("./builtins/read.zig").read;
 pub const socket = @import("./builtins/socket.zig").socket;
 pub const str = @import("./builtins/str.zig").str;
---
+++ src/builtins/open.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+const Helper = @import("./helper.zig");
+
+fn booleanOption(options: *Helper.Value, name: []const u8, default: bool) bool {
+    const option = options.v.RecordKind.get(name);
+
+    if (option == null or option.?.v != Helper.ValueKind.BoolKind) {
+        return default;
+    }
+
+    return option.?.v.BoolKind;
+}
+
+pub fn open(machine: *Helper.Machine, calleeAST: *Helper.Expression, argsAST: []*Helper.Expression) !void {
+    const path = (try Helper.getArgument(machine, calleeAST, argsAST, "path", 0, &[_]Helper.ValueKind{Helper.ValueValue.StringKind})).v.StringKind;
+    const options = try Helper.getArgument(machine, calleeAST, argsAST, "options", 1, &[_]Helper.ValueKind{ Helper.ValueValue.RecordKind, Helper.ValueValue.VoidKind });
+
+    if (options.v == Helper.ValueKind.VoidKind) {
+        try machine.memoryState.push(try machine.memoryState.newFileValue(std.fs.cwd().openFile(path, .{}) catch |err| return Helper.osError(machine, "open", err)));
+        return;
+    }
+
+    const readF = booleanOption(options, "read", false);
+    const writeF = booleanOption(options, "write", false);
+    const appendF = booleanOption(options, "append", false);
+    const truncateF = booleanOption(options, "truncate", false);
+    const createF = booleanOption(options, "create", false);
+
+    if (createF) {
+        try machine.memoryState.push(try machine.memoryState.newFileValue(std.fs.cwd().createFile(path, .{ .read = readF, .truncate = truncateF, .exclusive = true }) catch |err| return Helper.osError(machine, "open", err)));
+    } else {
+        const mode = if (readF and writeF) std.fs.File.OpenMode.read_write else if (readF) std.fs.File.OpenMode.read_only else std.fs.File.OpenMode.write_only;
+        var file = std.fs.cwd().openFile(path, .{ .mode = mode }) catch |err| return Helper.osError(machine, "open", err);
+
+        try machine.memoryState.push(try machine.memoryState.newFileValue(file));
+
+        if (appendF) {
+            file.seekFromEnd(0) catch |err| return Helper.osError(machine, "open", err);
+        }
+    }
+}
