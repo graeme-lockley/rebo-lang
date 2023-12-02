4b3d14ede01a9c9d71d45d34e04199bdffa24ff8
Graeme Lockley <graeme.lockley@gmail.com>
2023-12-02 07:46:48 +0200

    refactor: move builtin ls

---
+++ src/builtins/ls.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+const Helper = @import("./helper.zig");
+
+pub fn ls(machine: *Helper.Machine, calleeAST: *Helper.Expression, argsAST: []*Helper.Expression) !void {
+    const v = try Helper.getArgument(machine, calleeAST, argsAST, "path", 0, &[_]Helper.ValueKind{ Helper.ValueValue.StringKind, Helper.ValueValue.VoidKind });
+
+    const path = if (v.v == Helper.ValueKind.StringKind) v.v.StringKind else "./";
+    try machine.memoryState.pushEmptySequenceValue();
+
+    var dir = std.fs.cwd().openIterableDir(path, .{}) catch return;
+    defer dir.close();
+
+    const result = machine.memoryState.peek(0);
+
+    var it = dir.iterate();
+    while (it.next() catch return) |entry| {
+        if (std.mem.eql(u8, entry.name, ".") or std.mem.eql(u8, entry.name, "..")) {
+            continue;
+        }
+
+        const record = try machine.memoryState.newMapValue();
+        try result.v.SequenceKind.appendItem(record);
+
+        try record.v.RecordKind.set(machine.memoryState.allocator, "name", try machine.memoryState.newStringValue(entry.name));
+
+        const kind = switch (entry.kind) {
+            std.fs.IterableDir.Entry.Kind.block_device => "block_device",
+            std.fs.IterableDir.Entry.Kind.character_device => "character_device",
+            std.fs.IterableDir.Entry.Kind.directory => "directory",
+            std.fs.IterableDir.Entry.Kind.door => "door",
+            std.fs.IterableDir.Entry.Kind.event_port => "event_port",
+            std.fs.IterableDir.Entry.Kind.file => "file",
+            std.fs.IterableDir.Entry.Kind.named_pipe => "named_pipe",
+            std.fs.IterableDir.Entry.Kind.sym_link => "sym_link",
+            std.fs.IterableDir.Entry.Kind.unix_domain_socket => "unix_domain_socket",
+            std.fs.IterableDir.Entry.Kind.unknown => "unknown",
+            std.fs.IterableDir.Entry.Kind.whiteout => "whiteout",
+        };
+        try record.v.RecordKind.set(machine.memoryState.allocator, "kind", try machine.memoryState.newStringValue(kind));
+    }
+}
