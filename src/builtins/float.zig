17a50c0632907692b0ae979c9fc4c1ef231fcf62
Graeme Lockley <graeme.lockley@gmail.com>
2024-01-04 09:35:31 +0200

    refactor: remove dead code

--- src/builtins/float.zig
+++ src/builtins/float.zig
@@ -5,17 +5,11 @@ pub fn float(machine: *Helper.Machine, numberOfArgs: usize) !void {
     const v = try Helper.getArgument(machine, numberOfArgs, 0, &[_]Helper.ValueKind{Helper.ValueKind.StringKind});
     const d = if (numberOfArgs > 1) machine.memoryState.peek(numberOfArgs - 2) else machine.memoryState.unitValue.?;
 
-    switch (v.v) {
-        Helper.ValueValue.CharKind => try machine.memoryState.pushIntValue(@intCast(v.v.CharKind)),
-        Helper.ValueKind.StringKind => {
-            const literalFloat = std.fmt.parseFloat(Helper.FloatType, v.v.StringKind.slice()) catch {
-                try machine.memoryState.push(d);
-                return;
-            };
-            try machine.memoryState.pushFloatValue(literalFloat);
-        },
-        else => unreachable,
-    }
+    const literalFloat = std.fmt.parseFloat(Helper.FloatType, v.v.StringKind.slice()) catch {
+        try machine.memoryState.push(d);
+        return;
+    };
+    try machine.memoryState.pushFloatValue(literalFloat);
 }
 
 test "float" {
